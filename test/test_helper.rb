b42890e31383a17425fe1a177952442c36277639
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-04-18 14:15:10 +1200

    Ignore dummy application from coverage percentage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+SimpleCov.start do
+  add_filter 'test/dummy'
+end
 
 
 # Configure Rails Environment
