33cce0497a64b6c7d0c33e7f0dfc545bd37b1c53
Justin Workman <xtagon@gmail.com>
2018-10-12 17:16:57 -0700

    Use Mime[:text] for content type to fix deprecation in Rails 5.

--- app/controllers/new_relic_ping/health_controller.rb
+++ app/controllers/new_relic_ping/health_controller.rb
@@ -19,7 +19,8 @@ module NewRelicPing
 
     def send_response(status_msg, meta_info = {})
       write_headers(meta_info)
-      render :text => status_msg.to_s, :status => status_msg, :content_type => Mime::TEXT
+      content_type = (Rails::VERSION::MAJOR < 5 ? Mime::TEXT : Mime[:text])
+      render :text => status_msg.to_s, :status => status_msg, :content_type => content_type
     end
 
     def write_headers(values = {})
